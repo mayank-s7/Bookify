8abc7dad10ec655304a6a3baec8f794a243a81b6
mayank <mayank94378@gmail.com>
2023-11-21 17:07:44 +0530

    3 removed

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <FirebaseProvider>3
+      <FirebaseProvider>
         <App />
       </FirebaseProvider>
     </BrowserRouter>
